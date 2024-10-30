f26eba9d1fbb1f1e1789c15c590ef1b9c70d5208
Ivan Hlukhenkyi <gluhenkiy.i@gmail.com>
2024-10-30 16:16:22 +0200

    test

--- src/ATM/ATM.java
+++ src/ATM/ATM.java
@@ -1,2 +1,47 @@
-package ATM;public class ATM {
+package ATM;
+
+import javax.naming.InsufficientResourcesException;
+
+public class ATM {
+    private double balance;
+
+    public ATM(double balance) {
+        this.balance = balance;
+    }
+
+    public void withdraw(double amount) throws InsufficientFundsException, InvalidAmountException {
+        if (balance < amount){
+            throw new InsufficientFundsException("Not enough money to withdraw.");
+        }
+        else if(amount <= 0){
+            throw new InvalidAmountException("Amount of withdrawal should be positive.");
+        }
+        else{
+            balance -= amount;
+            System.out.println("Money has been withdrawn successfully.\nBalance: " + balance);
+        }
+    }
+    public void deposit(double amount) throws InvalidAmountException{
+        if (amount <= 0){
+            throw new InvalidAmountException("Amount of deposit should be positive.");
+        }
+        else{
+            balance += amount;
+            System.out.println("Deposit has been made successfully.\nBalance: " + balance);
+        }
+    }
+    public double checkBalance(){
+        return balance;
+    }
+}
+
+class InsufficientFundsException extends Exception{
+    public InsufficientFundsException(String message){
+        super(message);
+    }
+}
+class InvalidAmountException extends Exception{
+    public InvalidAmountException(String message){
+        super(message);
+    }
 }
--- src/ATM/Main.java
+++ src/ATM/Main.java
@@ -1,2 +1,12 @@
-package ATM;public class Main {
+package ATM;
+
+public class Main {
+    public static void main(String[] args) throws InsufficientFundsException, InvalidAmountException {
+        ATM atm = new ATM(2000);
+        atm.withdraw(500);
+//        atm.withdraw(2500); - InsufficientFundsException
+//        atm.deposit(0); - InvalidAmountException
+        atm.deposit(100);
+        System.out.println(atm.checkBalance());
+    }
 }
